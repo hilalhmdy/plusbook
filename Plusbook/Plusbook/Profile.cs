791301dc51445175941c7053d7512e366f8f95be
Matthaumar <matthaumar@gmail.com>
2021-03-25 04:11:16 +0700

    Memasukkan profile dan profilelist ke dalam project

--- Plusbook/Plusbook/Profile.cs
+++ Plusbook/Plusbook/Profile.cs
@@ -1,29 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Profile
 {
 	private string name;
-	private FriendList* friends_list;
+	private List<Profile> friends_list;
 
 	public Profile()
 	{
+		this.name = "Dummy";
+		this.friends_list = new List<Profile>();
 	}
-	public Profile(string)
+	public Profile(string Name)
 	{
+		this.name = Name;
+		this.friends_list = new List<Profile>();
 	}
-	public Profile(const Profile&)
+	public Profile(Profile Other)
 	{
+		this.name = Other.name;
+		this.friends_list = new List<Profile>();
+		foreach (var Prof in Other.friends_list) {
+			this.friends_list.Add(Prof);
+		}
 	}
-	public Profile& operator=(const Profile&)
+	public Profile copy()
 	{
+		Profile P2 = new Profile(this.name);
+		P2.friends_list = this.getFriendList();
+
+		return P2;
 	}
-	public ~Profile()
+	
+	public string getName() { return this.name; }
+	public List<Profile> getFriendList()
 	{
+		List<Profile> newFriends = new List<Profile>();
+
+		foreach (var Prof in this.friends_list) {
+			newFriends.Add(Prof);
+		}
+
+		return newFriends;
+	}
+	public void setName(string Name) { this.name = Name; }
+
+	public void addFriend(Profile NewFriend) 
+	{
+		this.friends_list.Add(NewFriend);
 	}
 
-	public string getName() { }
-	public void setName() { }
-	public FriendList getFriendList() { }
+	public bool isFriendsWith(string Name)
+	{
+		for (int i = 0; i < this.friends_list.Count; i++) {
+			if (this.friends_list[i].name == Name) {
+				return true;
+			}
+		}
 
-	public void addFriend(Profile*) { }
+		return false;
+	}
 }
--- Plusbook/Plusbook/ProfileList.cs
+++ Plusbook/Plusbook/ProfileList.cs
@@ -1,24 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class ProfileList
 {
-	private int num_of_profiles;
-	private Profile*;
+	private List<Profile> profiles;
 	
 	public ProfileList()
 	{
+		this.profiles = new List<Profile>();
 	}
-	public ProfileList(const ProfileList&)
+	public ProfileList(ProfileList Other)
 	{
-	}
-	public ProfileList& operator=(const ProfileList&)
-	{
-	}
-	public ~ProfileList()
-	{
+		this.profiles = new List<Profile>();
+		foreach (var Prof in Other.profiles) {
+			this.profiles.Add(Prof);
+		}
 	}
 
-	public void addProfile(string) { }
-	public bool isExist(string) { }
-	public void addConnection(string, string) { }
+	public void addProfile(string Name)
+	{
+		Profile newProfile = new Profile(Name);
+		this.profiles.Add(newProfile);
+	}
+	public bool isExist(string Name)
+	{
+		for (int i = 0; i < this.profiles.Count; i++) {
+			if (this.profiles[i].getName() == Name) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	public Profile getProfile(string Name) 
+	{
+		for (int i = 0; i < this.profiles.Count; i++) {
+			if (this.profiles[i].getName() == Name)
+			{
+				return this.profiles[i];
+			}
+		}
+
+		return new Profile();
+	}
+	public void addConnection(string A, string B)
+	{
+		Profile ProfA, ProfB;
+
+		if (!this.isExist(A)) {
+			ProfA = new Profile(A);
+			this.profiles.Add(ProfA);
+		}
+		if (!this.isExist(B)) {
+			ProfB = new Profile(B);
+			this.profiles.Add(ProfB);
+		}
+
+		ProfA = this.getProfile(A);
+		ProfB = this.getProfile(B);
+		if (!ProfA.isFriendsWith(B)) {
+			ProfA.addFriend(ProfB);
+		}
+		if (!ProfB.isFriendsWith(A)) {
+			ProfB.addFriend(ProfA);
+		}
+	}
 }
