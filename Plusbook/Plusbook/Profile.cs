9f1a71387ae474d967890445d6a64cc9ec82c77e
Matthaumar <matthaumar@gmail.com>
2021-03-24 10:33:53 +0700

    add class Profile, ProfileList, FriendList

---
+++ Plusbook/Plusbook/Profile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Profile
+{
+	private string name;
+	private FriendList* friends_list;
+
+	public Profile()
+	{
+	}
+	public Profile(string)
+	{
+	}
+	public Profile(const Profile&)
+	{
+	}
+	public Profile& operator=(const Profile&)
+	{
+	}
+	public ~Profile()
+	{
+	}
+
+	public string getName() { }
+	public void setName() { }
+	public FriendList getFriendList() { }
+
+	public void addFriend(Profile*) { }
+}
---
+++ Plusbook/Plusbook/ProfileList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class ProfileList
+{
+	private int num_of_profiles;
+	private Profile*;
+	
+	public ProfileList()
+	{
+	}
+	public ProfileList(const ProfileList&)
+	{
+	}
+	public ProfileList& operator=(const ProfileList&)
+	{
+	}
+	public ~ProfileList()
+	{
+	}
+
+	public void addProfile(string) { }
+	public bool isExist(string) { }
+	public void addConnection(string, string) { }
+}
